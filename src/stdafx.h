a93efb8b67a1d6bbcb3563f0bc148721ee58040c
zDark420 <g3829481@trbvm.com>
2015-05-19 22:14:42 -0700

    Create stdafx.h

---
+++ src/stdafx.h
@@ -0,0 +1,14 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h> //Windows SDK 8.1
+#include <TlHelp32.h>
+#include <iostream>
+#include <string>
+#include <random>
+#include "Decrypt.h"
+#include <stdio.h>
+#include <string.h>
+#include <sapi.h>//voice api
+#include <stringapiset.h>//required for MultiByteToWideChar
+using namespace std;
